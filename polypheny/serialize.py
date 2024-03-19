9d0782a463918a4859b02c4db02b59a51ad9a81e
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-03-19 13:56:29 +0100

    Update serializer.py

--- polypheny/serialize.py
+++ polypheny/serialize.py
@@ -8,44 +8,35 @@ def py2proto(value, v=None):
     if v is None:
         v = value_pb2.ProtoValue()
     if type(value) == bool:
-        # v.type = value_pb2.ProtoValue.ProtoValueType.BOOLEAN
         v.boolean.boolean = value
     elif type(value) == int:
         if -2 ** 31 <= value <= 2 ** 31 - 1:
-            # v.type = value_pb2.ProtoValue.ProtoValueType.INTEGER
             v.integer.integer = value
         elif -2 ** 63 <= value <= 2 ** 63 - 1:
-            # v.type = value_pb2.ProtoValue.ProtoValueType.LONG
             v.long.long = value
         else:
-            # v.type = value_pb2.ProtoValue.ProtoValueType.BIG_DECIMAL
             n = ((value.bit_length() - 1) // 8) + 1  # TODO: Does this work for negative numbers?
             v.big_decimal.unscaled_value = value.to_bytes(n, byteorder='big', signed=True)
             v.big_decimal.scale = 0
             v.big_decimal.precision = 0
             print(v.big_decimal)
-    elif type(value) == bytes:
-        # v.type = value_pb2.ProtoValue.ProtoValueType.BINARY
-        v.binary.binary = value
+    elif type(value) == float:
+        v.double.double = value
+        # TODO: Use BigDecimal as well?
     elif type(value) == datetime.date:
         diff = value - datetime.date(1970, 1, 1)
         v.date.date = diff.days
-    elif type(value) == float:
-        # v.type = value_pb2.ProtoValue.ProtoValueType.DOUBLE
-        v.double.double = value
-        # TODO: Use BigDecimal as well?
-    elif type(value) == str:
-        # v.type = value_pb2.ProtoValue.ProtoValueType.VARCHAR
-        v.string.string = value
     elif type(value) == datetime.time:
         v.time.time = (value.hour * 3600 + value.minute * 60 + value.second) * 1000 + value.microsecond * 10
     elif type(value) == datetime.datetime:
         v.timestamp.timestamp = int(value.timestamp() * 1000)
+    elif type(value) == str:
+        v.string.string = value
+    elif type(value) == bytes:
+        v.binary.binary = value
     elif value is None:
-        # v.type = value_pb2.ProtoValue.ProtoValueType.NULL
         v.null.CopyFrom(value_pb2.ProtoNull())
     elif type(value) == list:
-        # v.type = value_pb2.ProtoValue.ProtoValueType.LIST
         for element in value:
             v.list.values.append(py2proto(element))
     elif type(value) == dict:  # experiment to test the server with unset values
@@ -65,16 +56,23 @@ def proto2py(value):
         return value.integer.integer
     elif name == "long":
         return value.long.long
-    elif name == "binary":
-        return value.binary.binary
-    elif name == "date":
-        return datetime.date(1970, 1, 1) + datetime.timedelta(days=value.date.date)
-    elif name == "double":
-        return value.double.double
+    elif name == "big_decimal":
+        raw = value.big_decimal.unscaled_value
+        scale = value.big_decimal.scale
+        prec = value.big_decimal.precision
+
+        if scale > (2 ** 31) - 1:
+            scale = scale - 2 ** 32
+
+        i = int.from_bytes(raw, byteorder='big', signed=True)
+        i = i * 10 ** (-scale)
+        return round(i, prec + 1)  # TODO: Round Up/Down?
     elif name == "float":
         return value.float.float
-    elif name == "string":
-        return value.string.string
+    elif name == "double":
+        return value.double.double
+    elif name == "date":
+        return datetime.date(1970, 1, 1) + datetime.timedelta(days=value.date.date)
     elif name == "time":
         t = value.time.time
         print(t)
@@ -88,28 +86,14 @@ def proto2py(value):
         return datetime.time(hour, minute, second, microsecond=int(millis/10))
     elif name == "timestamp":
         return datetime.datetime.fromtimestamp(value.timestamp.timestamp / 1000, datetime.timezone.utc)
-    elif name == "null":
-        return None
-    elif name == "big_decimal":
-        # print(value)
-        raw = value.big_decimal.unscaled_value
-        scale = value.big_decimal.scale
-        prec = value.big_decimal.precision
-
-        if scale > (2 ** 31) - 1:
-            scale = scale - 2 ** 32
-
-        i = int.from_bytes(raw, byteorder='big', signed=True)
-        # print(f'i: {i}')
-        i = i * 10 ** (-scale)
-        # print(f'i: {i} {2**77}')
-        return round(i, prec + 1)  # TODO: Round Up/Down?
     elif name == "interval":
         raise NotImplementedError()
-    elif name == "user_defined_type":
-        raise NotImplementedError()
-    elif name == "file":
-        raise NotImplementedError()
+    elif name == "string":
+        return value.string.string
+    elif name == "binary":
+        return value.binary.binary
+    elif name == "null":
+        return None
     elif name == "list":
         return list(map(lambda v: proto2py(v), value.list.values))
     elif name == "map":
@@ -122,15 +106,5 @@ def proto2py(value):
             v = proto2py(entry.value)
             res[k] = v
         return res
-    elif name == "node":
-        raise NotImplementedError()
-    elif name == "edge":
-        raise NotImplementedError()
-    elif name == "path":
-        raise NotImplementedError()
-    elif name == "graph":
-        raise NotImplementedError()
-    elif name == "row_id":
-        raise NotImplementedError()
     else:
         raise RuntimeError("Unhandled value type")
