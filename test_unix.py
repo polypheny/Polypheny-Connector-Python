f17aeddf103543e058da3c903245cb1ad45f61f7
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-04-11 08:31:45 +0200

    Test connect without arguments as well

--- test_unix.py
+++ test_unix.py
@@ -3,13 +3,19 @@ import sys
 
 import polypheny
 
-def unix_connect():
-    con = polypheny.connect(os.path.expanduser("~/.polypheny/polypheny-prism.sock"), username='pa', password='', transport='unix')
+def unix_connect(args):
+    if args:
+        con = polypheny.connect(os.path.expanduser("~/.polypheny/polypheny-prism.sock"), username='pa', password='', transport='unix')
+    else:
+        con = polypheny.connect()
     con.close()
 
 def test_connect_unix():
     if sys.platform == 'win32':  # TODO: Once this works the documentation needs to be updated
         with pytest.raises(AttributeError):
-            unix_connect()
+            unix_connect(False)
+        with pytest.raises(AttributeError):
+            unix_connect(True)
     else:
-        unix_connect()
+        unix_connect(True)
+        unix_connect(False)
