f500abfefc62e910435634b48d6b64210a9ecaea
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-04-22 10:20:22 +0200

    Skip test before the hanging one

--- test_langs.py
+++ test_langs.py
@@ -4,6 +4,8 @@ import pytest
 from test_helper import con, cur, cur_with_data
 
 def test_cql(cur_with_data):
+    if sys.platform == 'win32':
+        pytest.skip()
     cur = cur_with_data
     cur.executeany('cql', "public.customers.id == 1 project public.customers.name")
     assert cur.fetchone()[0] == 'Maria'
