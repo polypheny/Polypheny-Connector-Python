826709125e1ee148efbc3414b7f4e83f4793c561
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-04-22 08:36:25 +0200

    Skip test

--- test_future.py
+++ test_future.py
@@ -2,6 +2,7 @@
 This file contains tests that do not work yet, but should work in future versions of Polypheny.
 """
 import datetime
+import sys
 
 import polypheny
 import pytest
@@ -26,6 +27,8 @@ def test_insert_double(cur):
     assert cur.fetchone() == [2, 3]
 
 def test_dynamic_text_parameter(cur):
+    if sys.platform == 'win32':
+        pytest.skip()
     cur.execute('DROP TABLE IF EXISTS t')
     cur.execute('CREATE TABLE t(i INTEGER NOT NULL, a TEXT NOT NULL, PRIMARY KEY(i))')
     with pytest.raises(polypheny.Error):
