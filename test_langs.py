b31ace628555dd11c4285c7fb3d71dae617c147c
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-04-22 09:17:46 +0200

    Skip test

--- test_langs.py
+++ test_langs.py
@@ -9,6 +9,8 @@ def test_cql(cur_with_data):
     assert cur.fetchone()[0] == 'Maria'
 
 def test_pig(cur_with_data):
+    if sys.platform == 'win32':
+        pytest.skip()
     cur = cur_with_data
     cur.executeany('pig', "A = LOAD 'customers'; B = FILTER A BY id == 1; DUMP B;")
     assert cur.fetchone()[1] == 'Maria'
