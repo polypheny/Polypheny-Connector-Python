36668546b7019f1e604ef7f47e74475a0c86ed21
Martin Vahlensieck <martin.vahlensieck@unibas.ch>
2024-04-24 10:57:00 +0200

    Uncomment tests

--- test_langs.py
+++ test_langs.py
@@ -4,16 +4,16 @@ import sys
 
 from test_helper import con, cur, cur_with_data
 
-#def test_cql(cur_with_data):
-#    if sys.platform == 'win32':
-#        pytest.skip()
-#    cur = cur_with_data
-#    cur.executeany('cql', "public.customers.id == 1 project public.customers.name")
-#    assert cur.fetchone()[0] == 'Maria'
-#
-#def test_pig(cur_with_data):
-#    if sys.platform == 'win32':
-#        pytest.skip()
-#    cur = cur_with_data
-#    cur.executeany('pig', "A = LOAD 'customers'; B = FILTER A BY id == 1; DUMP B;")
-#    assert cur.fetchone()[1] == 'Maria'
+def test_cql(cur_with_data):
+    if sys.platform == 'win32':
+        pytest.skip()
+    cur = cur_with_data
+    cur.executeany('cql', "public.customers.id == 1 project public.customers.name")
+    assert cur.fetchone()[0] == 'Maria'
+
+def test_pig(cur_with_data):
+    if sys.platform == 'win32':
+        pytest.skip()
+    cur = cur_with_data
+    cur.executeany('pig', "A = LOAD 'customers'; B = FILTER A BY id == 1; DUMP B;")
+    assert cur.fetchone()[1] == 'Maria'
