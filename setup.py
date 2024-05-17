549b9a7d1c0112243642b0b9d0e27d333d72b1c3
Marco Vogt <marco.vogt@unibas.ch>
2024-05-17 15:57:19 +0200

    Adjust publish workflow

--- setup.py
+++ setup.py
@@ -1,27 +1,42 @@
 import os
-import sys
-
 from setuptools import setup
 
-# Retrieve 'VERSION' environment variable, default to '0.0' if not found.
-version = os.getenv('RELEASE_VERSION', '0.0')
+version_file = 'polypheny-connector-version.txt'
 
-# Attempt to split the version number, default to '0' for both if it fails
-try:
-    major, minor = version.split('.')
-except ValueError:
-    major, minor = '0', '0'  # Default to '0.0' if the version isn't in a 'major.minor' format
+if not os.path.exists(version_file):
+    raise ValueError(f"Version file '{version_file}' not found. Please create the file with the version number.")
+
+with open(version_file, 'r') as f:
+    version = f.read().strip()
+
+print(f"Building version: {version}")
+
+if not version.startswith('v'):
+    raise ValueError(f"Invalid version format: {version}. Expected format 'v0.0.0'.")
+
+# Strip the 'v' prefix for the version
+version = version[1:]
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='polypheny',
-      version=version,
-      description='Driver for Polypheny',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages=['polypheny'],
-      install_requires=[
-          "polypheny-prism-api==1.9",
-      ],
-      )
+setup(
+    name='polypheny',
+    version=version,
+    description='Driver for Polypheny',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="The Polypheny Project",
+    author_email="mail@polypheny.org",
+    url="https://polypheny.com/",
+    project_urls={
+        "Documentation": "https://docs.polypheny.com/en/latest/drivers/python/overview",
+        "Code": "https://github.com/polypheny/Polypheny-Connector-Python",
+        "Issue tracker": "https://github.com/polypheny/Polypheny-DB/labels/A-python"
+    },
+    license="Apache License, Version 2.0",
+    packages=['polypheny'],
+    install_requires=[
+        "polypheny-prism-api==1.9",
+    ],
+)
